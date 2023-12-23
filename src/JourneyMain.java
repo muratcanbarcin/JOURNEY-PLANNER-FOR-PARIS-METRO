c1313ddedea45e2c001aca6475620a9bfb4d33a9
Murat Can Barçın <muratcanbarcin@gmail.com>
2023-12-23 17:10:34 +0300

    Graph built

--- src/JourneyMain.java
+++ src/JourneyMain.java
@@ -1,27 +1,32 @@
 import java.io.*;
 import java.util.ArrayList;
+import GraphPackage.*;
 
 public class JourneyMain {
+    private DirectedGraph metroGraph;
     private ArrayList<Route> routes;
     public JourneyMain() {
         this.routes = new ArrayList<>();
+        this.metroGraph = new DirectedGraph();
 
         String csvFile = "Paris_RER_Metro.csv";
         String line;
         String cvsSplitBy = ",";
         int countRoute =-2;
+        int countStation =-2;
 
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
 
-            while ((line = br.readLine()) != null) {
-                if (countRoute == -2) {
+            while ((line = br.readLine()) != null) { //Loop skipping first line
+                if (countStation == -2) {
+                    countStation+=1;
                     countRoute+=1;
                     continue;
                 }
-                // CSV dosyasındaki her satırı virgül ile ayır
+                //Separate each line in CSV file with comma
                 String[] data = line.split(cvsSplitBy);
 
-                // Verileri doğru değişkenlere atama
+                //Assign data to the correct variables
                 int stopID = Integer.parseInt(data[0]);
                 String stopName = data[1];
                 int arrivalTime = Integer.parseInt(data[2]);
@@ -31,20 +36,59 @@ public class JourneyMain {
                 String routeLong = data[6];
                 int routeType = Integer.parseInt(data[7]);
 
-                boolean flag = routes.contains(routeLong);
-
-                if(!flag){
-                    Route routeNew = new Route(arrivalTime,directionID,routeShort,routeLong,routeType);
+                //Has been route added to routes arraylist control
+                boolean flag = true;
+                for(Route route : routes){
+                    if(routeLong.equalsIgnoreCase(route.getRouteName())){
+                        flag = false;
+                    }
+                }
+                //Route add to routes arraylist if has not been ever
+                if(flag){
+                    Route routeNew = new Route(directionID,routeShort,routeLong,routeType);
+                    routeNew.setStations(new ArrayList<>());
                     routes.add(routeNew);
                     countRoute+=1;
+                    //System.out.println("Route Number:" + countRoute); //testPrint
                 }
+                //Station add to stations arraylist
                 Route tempRoute = routes.get(countRoute);
-                Route.Station s = tempRoute.new Station(stopID,stopName,stopSequence);
+                Route.Station s = tempRoute.new Station(arrivalTime,stopID,stopName,stopSequence);
                 tempRoute.addStation(s);
-                System.out.println(countRoute);
+                countStation+=1;
+              //  System.out.println("Station Number:" + countStation); //testPrint
+
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+       buildGraph();
+       metroGraph.print();
+    }
+    public void buildGraph() {
+        for (Route route : routes) {
+            for (int i = 0; i < route.getStations().size() - 1; i++) {
+                String sourceStation = route.getStations().get(i).getStopName();
+                String destinationStation = route.getStations().get(i + 1).getStopName();
+                int weight = Math.abs(route.getStations().get(i).getArrivalTime() - route.getStations().get(i+1).getArrivalTime());
+
+                metroGraph.addEdge(sourceStation, destinationStation, weight);
+            }
+        }
+    }
+    public void displayRoutesAndStations() { //test display method
+        System.out.println("Routes:");
+
+        for (Route route : routes) {
+            System.out.println("Route: " + route.getRouteName());
+            System.out.println("Stations:");
+
+            for (Route.Station station : route.getStations()) {
+                System.out.println("  Station: " + station.getStopName());
+            }
+
+            System.out.println("------");
         }
     }
 }
--- src/Route.java
+++ src/Route.java
@@ -1,19 +1,56 @@
 import java.util.ArrayList;
 
 public class Route {
- private int arrivalTime;
  private int directionID;
  private String routeID;
  private String routeName;
  private int routeType;
  private ArrayList<Station> stations;
 
-    public Route(int arrivalTime, int directionID, String routeID, String routeName, int routeType) {
-        this.arrivalTime = arrivalTime;
+    public Route(int directionID, String routeID, String routeName, int routeType) {
         this.directionID = directionID;
         this.routeID = routeID;
         this.routeName = routeName;
         this.routeType = routeType;
+    }
+    public int getDirectionID() {
+        return directionID;
+    }
+
+    public void setDirectionID(int directionID) {
+        this.directionID = directionID;
+    }
+
+    public String getRouteID() {
+        return routeID;
+    }
+
+    public void setRouteID(String routeID) {
+        this.routeID = routeID;
+    }
+
+    public String getRouteName() {
+        return routeName;
+    }
+
+    public void setRouteName(String routeName) {
+        this.routeName = routeName;
+    }
+
+    public int getRouteType() {
+        return routeType;
+    }
+
+    public void setRouteType(int routeType) {
+        this.routeType = routeType;
+    }
+
+    public ArrayList<Station> getStations() {
+        return stations;
+    }
+
+    public void setStations(ArrayList<Station> stations) {
+        this.stations = stations;
     }
 
     public void addStation(Station s){
@@ -21,13 +58,47 @@ public class Route {
     }
 
     protected class Station {
+        private int arrivalTime;
         private int stopID;
         private String stopName;
         private int stopSequence;
 
-        public Station(int stopID, String stopName, int stopSequence) {
+        public Station(int arrivalTime,int stopID, String stopName, int stopSequence) {
             this.stopID = stopID;
             this.stopName = stopName;
+            this.stopSequence = stopSequence;
+            this.arrivalTime = arrivalTime;
+
+        }
+        public int getArrivalTime() {
+            return arrivalTime;
+        }
+
+        public void setArrivalTime(int arrivalTime) {
+            this.arrivalTime = arrivalTime;
+        }
+
+        public int getStopID() {
+            return stopID;
+        }
+
+        public void setStopID(int stopID) {
+            this.stopID = stopID;
+        }
+
+        public String getStopName() {
+            return stopName;
+        }
+
+        public void setStopName(String stopName) {
+            this.stopName = stopName;
+        }
+
+        public int getStopSequence() {
+            return stopSequence;
+        }
+
+        public void setStopSequence(int stopSequence) {
             this.stopSequence = stopSequence;
         }
     }
